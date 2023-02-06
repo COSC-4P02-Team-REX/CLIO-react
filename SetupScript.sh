00362459f7134685bfc9f44320036e90ee0dfb9c
Parth Patel <74056617+Parth-1612@users.noreply.github.com>
2023-02-06 11:18:04 -0500

    Update SetupScript.sh

--- SetupScript.sh
+++ SetupScript.sh
@@ -19,13 +19,16 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 rvm install 2.7.6
 rvm alias create default 2.7.6
 
+
+#Install Cocoapods
+sudo gem install cocoapods
+
 # Install Gem dependencies
 bundle install 
 # Install NPM dependencies
 npm install
 
-#Install Cocoapods
-sudo gem install cocoapods
+
 
 # Install CocoaPods dependencies
 cd ios
